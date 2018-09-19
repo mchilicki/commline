52c960c24480cc2d7a727d3315bfeca31924fccf
Marcin Chilicki <kipic96@gmail.com>
2018-09-19 16:53:43 +0200

    Start Ninject Dependency Injection

---
+++ Chilicki.Commline/Chilicki.Commline.UserInterface/App_Start/NinjectResolver.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chilicki.Commline.UserInterface.App_Start
+{
+    public class NinjectResolver : System.Web.Mvc.IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectResolver()
+        {
+            _kernel = new StandardKernel();
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+    }
+}
--- Chilicki.Commline/Chilicki.Commline.UserInterface/Global.asax.cs
+++ Chilicki.Commline/Chilicki.Commline.UserInterface/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chilicki.Commline.UserInterface.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace Chilicki.Commline.UserInterface
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            DependencyResolver.SetResolver(new NinjectResolver());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
